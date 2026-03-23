87e104fa83fc35d9fcc84728e855a0a8d886d964
PTaibo <p.taibo@udc.es>
2026-03-23 16:59:21 +0100

    Use different number of bits per block in bench

--- src/scripts/benchmark_run.sh
+++ src/scripts/benchmark_run.sh
@@ -1,24 +1,42 @@
 #!/bin/bash
 
+PURPLE='\033[0;35m'
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No colour
+
+wordBits=(8 32 64)
+bitsPerBlk=(3 600)
+wordMult=(1 2 3 4)
+
 cp ./bitmap.h ./bitmap.h.bckp
-# Change word_t to uint8_t
-echo "8 bit word"
-sed -i '/using word_t = / s/uint[0-9]\+_t/uint8_t/' ./bitmap.h > /dev/null
-make cleanall > /dev/null
-make cbench &> /dev/null
-./benchmark.elf
-# Change word_t to uint32_t
-echo "32 bit word"
-sed -i '/using word_t = / s/uint[0-9]\+_t/uint32_t/' ./bitmap.h > /dev/null
-make cleanall > /dev/null
-make cbench &> /dev/null
-./benchmark.elf
-# Change word_t to uint64_t
-echo "64 bit word"
-sed -i '/using word_t = / s/uint[0-9]\+_t/uint64_t/' ./bitmap.h > /dev/null
-make cleanall > /dev/null
-make cbench &> /dev/null
-./benchmark.elf
+cp ./bitmap.cpp ./bitmap.cpp.bckp
+
+for bits in "${wordBits[@]}"; do
+    echo "$bits bit word"
+    sed -i "/using word_t = / s/uint[0-9]\+_t/uint${bits}_t/" ./bitmap.h > /dev/null
+
+    for blk in "${bitsPerBlk[@]}"; do
+        echo "$blk bits per rank block"
+        sed -i "/#define RANKBLK / s/[0-9]\+/${blk}/" ./bitmap.cpp
+
+        make cleanall > /dev/null
+        make cbench &> /dev/null
+        ./benchmark.elf
+    done
+
+    for words in "${wordMult[@]}"; do
+        echo "$words word per rank block"
+        sed -i "/#define RANKBLK / s/[0-9]\+/$((${blk}*${bits}))/" ./bitmap.cpp
+
+        make cleanall > /dev/null
+        make cbench &> /dev/null
+        ./benchmark.elf
+    done
+
+    echo "-------------------"
+done
 
 mv ./bitmap.h.bckp ./bitmap.h
+mv ./bitmap.cpp.bckp ./bitmap.cpp
 
