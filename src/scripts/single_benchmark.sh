3e761ab1dc730c8b6b4b6314a16ee494f9a3c482
PTaibo <p.taibo@udc.es>
2026-07-02 13:53:14 +0200

    feat: run only 32b benchmarks

--- src/scripts/single_benchmark.sh
+++ src/scripts/single_benchmark.sh
@@ -7,37 +7,21 @@ BLUE='\033[0;34m'
 CYAN='\033[0;36m'
 NC='\033[0m' # No colour
 
-wordBits=(32 64)
-# wordMult=(1 2 3 4 5 10 15 20 25 30 35 100 200)
 wordMult=(1 2 10 20 30 100)
 
 cp $CODE/bitmap.h $CODE/bitmap.h.bckp
 cp $CODE/bitmap.cpp $CODE/bitmap.cpp.bckp
 
-for bits in "${wordBits[@]}"; do
-    echo "$bits bit word"
-    sed -i "/using word_t = / s/uint[0-9]\+_t/uint${bits}_t/" $CODE/bitmap.h > /dev/null
+for words in "${wordMult[@]}"; do
+    echo "$words word per rank block"
+    sed -i "/#define RANKBLK / s/[0-9]\+/${words}/" $CODE/bitmap.cpp
 
-    for blk in "${bitsPerBlk[@]}"; do
-        echo "$blk bits per rank block"
-        sed -i "/#define RANKBLK / s/[0-9]\+/${blk}/" $CODE/bitmap.cpp
-
-        make -f $CODE/Makefile cleanall > /dev/null
-        make -f $CODE/Makefile cbench &> /dev/null
-        $CODE/benchmark.elf $1
-    done
-
-    for words in "${wordMult[@]}"; do
-        echo "$words word per rank block"
-        sed -i "/#define RANKBLK / s/[0-9]\+/${words}/" $CODE/bitmap.cpp
-
-        make -f $CODE/Makefile cleanall > /dev/null
-        make -f $CODE/Makefile cbench &> /dev/null
-        $CODE/benchmark.elf $1
-    done
-
-    echo "-------------------"
+    make -f $CODE/Makefile cleanall > /dev/null
+    make -f $CODE/Makefile cbench &> /dev/null
+    $CODE/benchmark.elf $1
 done
+
+echo "-------------------"
 
 mv $CODE/bitmap.h.bckp $CODE/bitmap.h
 mv $CODE/bitmap.cpp.bckp $CODE/bitmap.cpp
